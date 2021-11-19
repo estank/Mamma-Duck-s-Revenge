a9c4cd1a0ebeae89591fb4b7bf9af2e425077daf
anagha <hareraa2@vcu.edu>
2021-11-19 11:32:21 -0500

    commit 8

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    //anagha commit 7
+    //anagha commit 8
     //evan commit
 }
