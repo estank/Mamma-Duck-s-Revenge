f4406656e5a20d651a45baedcef6d2d13982cb86
anagha <hareraa2@vcu.edu>
2021-11-19 17:31:13 -0500

    unnecessary files 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    //anagha commit 9
+    //anagha commit 10
     //evan commit
     //Mary commit
 }
