4d6bc4e50fea2980ec2e3d496009f912ee3f9d90
anagha <hareraa2@vcu.edu>
2021-12-13 23:49:21 -0500

    bitmapbank.java

---
+++ app/src/androidTest/java/com/example/myapplication/BitmapBank.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class BitmapBank {
+    Bitmap background;
+    Bitmap player;
+    Bitmap egg;
+    Bitmap log;
+
+    public BitmapBank(Resources res) {
+        background = BitmapFactory.decodeResource(res, R.drawable.forest);
+        player = BitmapFactory.decodeResource(res, R.drawable.duck_walking);
+        egg = BitmapFactory.decodeResource(res, R.drawable.egg);
+        log = BitmapFactory.decodeResource(res, R.drawable.log);
+    }
+
+    //background
+    public Bitmap getBackground() {
+        return background;
+    }
+    public int getBackgroundWidth() {
+        return background.getWidth();
+    }
+    public int getBackgroundHeight() {
+        return background.getHeight();
+    }
+
+    //player
+    public Bitmap getPlayer() {
+        return player;
+    }
+    public int getPlayerWidth() {
+        return player.getWidth();
+    }
+    public int getPlayerHeight() {
+        return player.getHeight();
+    }
+
+    //egg
+    public Bitmap getEgg() {
+        return egg;
+    }
+    public int getEggWidth() {
+        return egg.getWidth();
+    }
+    public int getEggHeight() {
+        return egg.getHeight();
+    }
+
+    //log
+    public Bitmap getLog() {
+        return log;
+    }
+    public int getLogWidth() {
+        return log.getWidth();
+    }
+    public int getLogHeight() {
+        return log.getHeight();
+    }
+
+    public Bitmap scaleImage(Bitmap bitmap) {
+        float widthHeightRatio = getBackgroundWidth() / getBackgroundHeight();
+        int backgroundScaleWidth = (int) widthHeightRatio * AppConstants.SCREEN_HEIGHT;
+        return Bitmap.createScaledBitmap(bitmap, backgroundScaleWidth, AppConstants.SCREEN_WIDTH, true);
+    }
+}
