092d8ec9246dfd392885068517cebebacda36ca5
anagha <hareraa2@vcu.edu>
2021-12-13 23:07:35 -0500

    added more params to gamemode

--- app/src/main/java/com/example/myapplication/GameMode.java
+++ app/src/main/java/com/example/myapplication/GameMode.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Point;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 public class GameMode extends AppCompatActivity {
 
@@ -14,6 +16,14 @@ public class GameMode extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppConstants.gameActivityContext = this;
+        RelativeLayout layout = new RelativeLayout(this);
+        layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        gameView = new GameView(this);
+        layout.addView(gameView);
+        setContentView(layout);
+
         setContentView(R.layout.activity_game_mode);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
